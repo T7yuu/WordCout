d459aa835ff5cb388430c536c4f5679d2c9f2dd1
tangqiuyu <1436255376@qq.com>
2023-05-05 12:41:08 +0800

    updata2

--- WordCount.cpp
+++ WordCount.cpp
@@ -15,7 +15,8 @@ int main()
 	while((ch=fgetc(f))!=EOF)
 	{
 		countStr++;
-		if(strcmp(ch," ")==0||strcmp(ch,",")==0)
+		//if((*ch==" ")||(*ch==","))
+		if((ch!=32)||(ch!=44))
 			countWord++;
 	}
 
@@ -29,3 +30,69 @@ int main()
 	fclose(f);
 	f=NULL;
 }
+/*
+void FileRead(char filename[]){
+
+FILE* fp;//必须要定义的指针
+
+int i=0，counter=0；
+
+int ch;
+
+char str[14];//用于.txt文件按词读取，即暂存器
+
+fp = fopen(filename, "r");//打开文件夹
+
+do{
+
+//读出词
+
+do
+
+{
+
+ch=fgetc(fp);
+
+if((ch>=97&&ch<=122)||(ch>=65&&ch<=90))//只有ch为字母时才录入
+
+{
+
+str[counter]=(char)ch;
+
+counter++;
+
+} }while(ch!='.'&&ch!='{'&&ch!='}'&&ch!='('&&ch!=')'&&ch!='['&&ch!=']'&&ch!=32&&ch!=59&&ch!=-1); //上为判定是否录入一完整词的判定结束条件
+
+printf("str:%s\n",str);
+
+/*
+
+这中间可以编写一些关于处理字符串的操作
+
+*/
+
+//清空暂存的数组
+/*
+for(i=0;i<14;j++)
+
+str[i]=0;
+
+counter=0;
+
+}while(ch!=EOF);//当文件读完时结束
+
+}
+
+void main()
+
+{
+
+char filename1[12];
+
+printf("请输入该文件名");
+
+scanf("%s",&filename1);
+
+FileRead(filename1);
+
+}*/
