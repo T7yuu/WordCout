02efff52e25ef2e64747500b876447b2aba1f3bd
tangqiuyu <1436255376@qq.com>
2023-05-05 12:10:03 +0800

    updata1

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,1 +1,31 @@
-ooo 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char jud[3];
+	char text[20];
+	int countStr=0;
+	int countWord=1;
+	char ch;
+	scanf("%s %s",jud,text);
+	FILE * f = fopen (text, "r+");
+	if(f == NULL)
+		perror("fopen");
+		
+	while((ch=fgetc(f))!=EOF)
+	{
+		countStr++;
+		if(strcmp(ch," ")==0||strcmp(ch,",")==0)
+			countWord++;
+	}
+
+	if(strcmp(jud,"-c")==0)
+	{
+		printf("%d",countStr);
+	}
+	else if(strcmp(jud,"-w")==0)
+		printf("%d",countWord);
+	
+	fclose(f);
+	f=NULL;
+}
